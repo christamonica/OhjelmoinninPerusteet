b7bc0503988fcb84578e07262a2abdec5dac1d2e
christamonica <christa.selin@centria.fi>
2025-12-16 21:50:35 +0200

    viikko42.csv sisältö lisätty ja luotu viikko5A.py

---
+++ Viikko5/A/viikko5A.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025 Christa Selin
+# License: MIT
+
+import os
+from datetime import datetime, date
+from typing import List
+
+# Asetetaan työskentelyhakemisto nykyiseen kansioon
+os.chdir(os.path.dirname(__file__)) 
